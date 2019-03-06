02e8d8b20a590a8bb8f8a4007cdbf56e48c039fd
Karen Kepner <kkepner14 (at) gmail.com>
2019-03-06 10:41:13 -0700

    add second animal picture to click on with name

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 let cat = document.querySelector('#catSpace');
-let clickCount = 0;
-let count = document.querySelector('#clickCount');
+let dog = document.querySelector('#dogSpace');
+let clickCount1 = 0;
+let clickCount2 = 0;
+
+let count1 = document.querySelector('#clickCount1');
+let count2 = document.querySelector('#clickCount2');
+
+let catName1 = 'Ollie';
+let catName2 = 'Amigo';
 
 cat.addEventListener('click', function() {
-  clickCount++;
-  count.innerHTML = clickCount;
+  clickCount1++;
+  count1.innerHTML = clickCount1;
 }, false);
 
+dog.addEventListener('click', function() {
+  clickCount2++;
+  count2.innerHTML = clickCount2;
+}, false);
+
+let name1 = document.querySelector('#name1');
+let name2 = document.querySelector('#name2');
+name1.innerHTML = catName1;
+name2.innerHTML = catName2;
