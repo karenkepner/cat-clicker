f61a94505a078516cc360ab289c068eaf4b144a8
Karen Kepner <kkepner14 (at) gmail.com>
2019-03-12 07:56:26 -0600

    too many cats, too many clicks

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-let cat = document.querySelector('#catSpace');
-let dog = document.querySelector('#dogSpace');
-let clickCount1 = 0;
-let clickCount2 = 0;
-
-let count1 = document.querySelector('#clickCount1');
-let count2 = document.querySelector('#clickCount2');
-
-let catName1 = 'Ollie';
-let catName2 = 'Amigo';
 
 let petInfo = [
   { name:   'Ollie',
@@ -34,35 +24,10 @@ let petInfo = [
   { name:   'Category3',
     image:  'category3.jpg',
     count:  0 }
-]
-
-cat.addEventListener('click', function() {
-  clickCount1++;
-  count1.innerHTML = clickCount1;
-}, false);
-
-dog.addEventListener('click', function() {
-  clickCount2++;
-  count2.innerHTML = clickCount2;
-}, false);
-
-// let name1 = document.querySelector('#name1');
-// let name2 = document.querySelector('#name2');
-// name1.innerHTML = catName1;
-// name2.innerHTML = catName2;
-
-// class Cat {
-//   constructor(name, image) {
-//     this.name = name;
-//     this.image = image;
-//     this.count = 0;
-//   }
-//   //use this if i need methods.
-// }
+];
 
 let list = document.querySelector('#petList');
-//populate the list of cats to choose from on pageload with iife
-//template literal!
+//populate the list of pets to choose from on pageload with iife
 (function buildList() {
     for(let i = 0; i < petInfo.length; i++) {
       list.insertAdjacentHTML('afterend', `<div class="pet" data-name="${petInfo[i].name}" data-count="${petInfo[i].count}">${petInfo[i].name}</div>`);
@@ -78,7 +43,7 @@ let list = document.querySelector('#petList');
       let et = event.target;
       let petName = et.dataset.name;
       let clickCount = et.dataset.count;
-      //then take that target (the clicked item) and use it to build the clickable cat.
+      //then take that target (the clicked item) and use it to build the clickable pet.
       addPet(petName, clickCount);
     })
   }
@@ -96,15 +61,25 @@ function addPet(name, count) {
                       </div>
                       <img class="space" src="images/${name}.jpg" alt="picture of ${name}" />`;
   putPetHere.prepend(yard);
-  // collectClicks();
   let clickable = document.querySelectorAll('.space');
   let clickableArray = Array.from(clickable);
   clickableArray.forEach(function(array) {
     array.addEventListener('click', function(event) {
       let et = event.target;
+      console.log('event target: ', et)
       count++;
       let counter = `#${name}Count`;
+      console.log('counter: ', counter)
       document.querySelector(counter).innerHTML = count;
     })
   })
+  // for (let i = 0; i < clickableArray.length; i++) {
+  //   clickableArray[i].addEventListener('click', function() {
+  //     console.log('clicked')
+  //     petInfo[i].count = count + 1;
+  //     let counter = `#${name}Count`;
+  //     console.log('counter: ', counter)
+  //     document.querySelector(counter).innerHTML = petInfo[i].count;
+  //   })
+  // }
 }
